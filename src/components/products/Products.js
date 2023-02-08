c0074b9d7775e5a4edbc1142132b6ab15beb0c12
AbbosxojaMarufxojayev <abbosxojamarufxojayev07@gmail.com>
2023-02-08 20:35:46 +0500

    something

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,20 +1,40 @@
 import React from 'react'
 import "./Products.css"
 import { BsHeart } from "react-icons/bs"
+import { AiFillHeart } from "react-icons/ai"
 
 import {PRODUCTS__DATA} from "../../static"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { ADD_TO_LIKE } from "../../context/action/actionType"
 import { Link } from "react-router-dom"
 
 function Products() {
   const dispatch = useDispatch()
+  const likes = useSelector(s=>s.heart)
+
+
+
+
+  const addHeart = (item)=>{
+    let index = likes.findIndex(i=> i.id === item.id)
+    if(index > -1){
+      return
+    }
+
+    dispatch({type: ADD_TO_LIKE, payload: item})
+  }
   return (
     <div>
       <div className="products__container">
       {
         PRODUCTS__DATA?.map((items, inx)=> <div key={inx} className="products__cards">
-        <div className="products_img__part"><Link to={`products/${items?.id}`}><img src={items?.url} alt="" /></Link><div className="like__icon"><BsHeart onClick={()=> dispatch({type: ADD_TO_LIKE, payload: items})}/></div></div>
+        <div className="products_img__part"><Link to={`products/${items?.id}`}><img src={items?.url} alt="" /></Link>
+        <div onClick={()=> addHeart(items)} className="like__icon">
+          {
+            likes?.some(i=> i.id === items.id) ? <AiFillHeart/> : <BsHeart/>
+          }
+        </div>
+        </div>
         <div className="products_text__part">
           <p className="name">{items?.title}</p>
           <p className="price">{items?.price} so'm</p>
